ebc7c35f3f2a078471cd5bd14acdc8b86926bcb5
Robert Yearling <laserguy@mac.com>
2020-12-11 13:11:13 -0600

    First Upload of all files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RandPass.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
